4324621a2ce6000072d01bec0e6adfd28fe1d098
olovm <olov.mckie@ub.uu.se>
2022-10-07 15:19:12 +0000

    splitting testspies to different projects

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,14 +3,4 @@
  */
 module se.uu.ub.cora.testspies {
 
-	requires org.testng;
-	requires se.uu.ub.cora.data;
-	requires se.uu.ub.cora.spider;
-	requires se.uu.ub.cora.httphandler;
-	requires transitive se.uu.ub.cora.testutils;
-
-	exports se.uu.ub.cora.testspies.data;
-	exports se.uu.ub.cora.testspies.spider;
-	exports se.uu.ub.cora.testspies.httphandler;
-	exports se.uu.ub.cora.testspies.storage;
 }
--- src/main/java/se/uu/ub/cora/testspies/spider/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Test spies for cora-spider.
- * <p>
- * Do NOT use these in cora-spider as that creates a circular dependency.
- */
-package se.uu.ub.cora.testspies.spider;
